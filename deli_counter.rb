e85cb4943f291f4b5ffc6557ccffd8ab6b18528c
Fede McMac <federica.macchi3@gmail.com>
2019-05-15 15:52:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,11 @@ end
 # If line is not empty add person to the end of the line, print message
 
 def take_a_number(queue, name)
-  
+  if queue == []
+    queue << name
+    message = "Welcome, #{name}. You are number 1 in line."
+    puts message
+  else
     queue.push(name)
     number_in_queue = queue.length
     message = "Welcome, #{name}. You are number #{number_in_queue} in line."
